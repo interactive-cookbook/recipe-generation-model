3c61ab333a2fda2a0a720653e29b52e5198fde5e
Katharina Stein <36165516+kastein@users.noreply.github.com>
2022-11-04 18:00:44 +0100

    first commit

---
+++ dataset_reader.py
@@ -0,0 +1,118 @@
+import os
+
+import torch
+from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
+
+
+# Taken from amrlib code
+class AMRDataset(Dataset):
+    def __init__(self, encodings, sents):
+        self.encodings = encodings
+        self.sents = sents
+
+    def __getitem__(self, idx):
+        return {k:v[idx] for k, v in self.encodings.items()}
+
+    def __len__(self):
+        return len(self.encodings['input_ids'])
+
+
+def build_dataset(tokenizer: PreTrainedTokenizer, data_path, context_len: int,
+                  max_in_length: int, max_out_length, linearization: str = 'penman') -> Dataset:
+
+    data_set_entries = read_data_set(data_path, context_len, linearization)
+    # TODO maybe change special token
+    contextualized_input = [f'{c} <GRAPH> {g}' for (c, g) in zip(data_set_entries['context'], data_set_entries['graph'])]
+    input_seqs = ['%s' % cont_gr for cont_gr in contextualized_input]
+    target_seqs = ['%s' % sent for sent in data_set_entries['sent']]
+
+    # Encode input and target using tokenizer
+    # original code had also truncation = True and max_length = X
+    # TODO: decide what to do -> does not work to have padding and return_overflowing_tokens but not max_length and truncation
+    input_encodings = tokenizer.batch_encode_plus(input_seqs,
+                                                  padding=True,
+                                                  max_length=max_in_length,
+                                                  truncation=True,
+                                                  return_overflowing_tokens=True)
+    target_encodings = tokenizer.batch_encode_plus(target_seqs,
+                                                   padding=True,
+                                                   max_length= max_out_length,
+                                                   truncation=True,
+                                                   return_overflowing_tokens=True)
+    bi = set()
+    for i, (ie, te) in enumerate(
+            zip(input_encodings['num_truncated_tokens'], target_encodings['num_truncated_tokens'])):
+        if ie > 0 or te > 0:
+            bi.add(i)
+    num_trunc = len(bi)
+    encodings = {'input_ids': torch.LongTensor(input_encodings['input_ids']),
+                 'attention_mask': torch.LongTensor(input_encodings['attention_mask']),
+                 'target_ids': torch.LongTensor(target_encodings['input_ids']),
+                 'target_attention_mask': torch.LongTensor(target_encodings['attention_mask'])}
+
+    data_set = AMRDataset(encodings, data_set_entries['sent'])
+    return data_set
+
+
+def read_data_set(data_path, context_len: int, linearization:str = 'penman'):
+
+    if os.path.isdir(data_path):
+        data_set_entries = {'sent': [], 'graph': [], 'context': []}
+        for document in os.listdir(data_path):
+            document_entries = read_document(os.path.join(data_path, document), context_len, linearization)
+            data_set_entries['sent'].extend(document_entries['sent'])
+            data_set_entries['graph'].extend(document_entries['graph'])
+            data_set_entries['context'].extend(document_entries['context'])
+    # enable also reading in only one train / val / test file, e.g. with context_len = 0 to get same behavior as
+    # for original model
+    else:
+        data_set_entries = read_document(data_path, context_len, linearization)
+
+    return data_set_entries
+
+
+# based on amr_loading.py from amrlib but modified
+def read_document(document_file, context_len, linearization:str = 'penman'):
+
+    document_dict = {'sent': [], 'graph': [], 'context': []}
+    ordered_sentences = []
+
+    with open(document_file, 'r', encoding='utf-8') as doc_f:
+        document = doc_f.read()
+
+    for amr_data in document.split('\n\n'):
+        graph_str = []
+        sentence = None
+        if not amr_data:
+            continue
+        elif amr_data.startswith('# AMR release'):
+            continue
+        for line in amr_data.split('\n'):
+            if line.startswith('# ::snt'):
+                sentence = line[len('# ::snt'):].strip()
+            elif line.startswith('# '):
+                continue
+            else:
+                graph_str.append(line.strip())
+
+        if sentence and graph_str:
+            if linearization != 'penman':
+                raise NotImplemented
+
+            document_dict['sent'].append(sentence)
+            document_dict['graph'].append(' '.join(graph_str))
+
+            if context_len > 0:
+                context = ordered_sentences[-context_len:]
+            else:
+                context = []
+            # concatenate the context sentences; if first sentence then context will be empty string
+            document_dict['context'].append(' '.join(context))
+            ordered_sentences.append(sentence)
+
+    return document_dict
+
+
+
+
---
+++ training.py
@@ -0,0 +1,89 @@
+import os
+import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer, Trainer
+from transformers import TrainingArguments
+import json
+import torch.cuda
+
+from dataset_reader import build_dataset
+
+
+# Taken from amrlib code
+class T2TDataCollator:
+    def __call__(self, batch):
+        input_ids = torch.stack([example['input_ids'] for example in batch])
+        lm_labels = torch.stack([example['target_ids'] for example in batch])
+        lm_labels[lm_labels[:, :] == 0] = -100      # -100 is ignored by the loss -> used for padding ids
+        attention_mask = torch.stack([example['attention_mask'] for example in batch])
+        decoder_attention_mask = torch.stack([example['target_attention_mask'] for example in batch])
+
+        collated_data = {'input_ids': input_ids, 'attention_mask': attention_mask,
+                         'labels': lm_labels, 'decoder_attention_mask': decoder_attention_mask}
+
+        return collated_data
+
+
+def train_generation_model(config_file):
+    with open(config_file) as c:
+        config_args = json.load(c)
+
+    torch.cuda.empty_cache()
+
+    training_args = TrainingArguments(**config_args['train_args'])
+
+    run_training_loop(config_args['gen_args'], training_args)
+
+
+def run_training_loop(general_config: dict, train_config: TrainingArguments):
+
+    print("---------- Loading Model and Tokenizer ----------")
+    model_path = general_config['model_name_or_path']
+    try:
+        tokenizer_path = general_config['tokenizer_name_or_path']
+    except KeyError:
+        tokenizer_path = general_config['model_name_or_path']
+    model = T5ForConditionalGeneration.from_pretrained(model_path)
+    tokenizer = T5Tokenizer.from_pretrained(tokenizer_path)
+
+    # Add the new special token
+    # information from here: https://github.com/huggingface/transformers/issues/8706
+    new_special_token = {'additional_special_tokens': ['<GRAPH>']}
+    tokenizer.add_special_tokens(new_special_token)
+    model.resize_token_embeddings(len(tokenizer))
+
+    # These get saved together with the model in a config.json file to store them
+    model.config.task_specific_params = {'translation_cond_amr_to_text': general_config}
+
+    print("---------- Reading the Data ----------")
+    train_data_path = os.path.join(general_config['corpus_dir'], general_config['train_path'])
+    valid_data_path = os.path.join(general_config['corpus_dir'], general_config['valid_path'])
+    train_dataset = build_dataset(tokenizer=tokenizer,
+                                  data_path=train_data_path,
+                                  context_len=general_config['context_len'],
+                                  linearization=general_config['linearization'],
+                                  max_in_length=general_config['max_in_len'],
+                                  max_out_length=general_config['max_out_len'])
+    print(f'Loaded Training data set of {len(train_dataset)} instances.')
+    valid_dataset = build_dataset(tokenizer=tokenizer,
+                                  data_path=valid_data_path,
+                                  context_len=general_config['context_len'],
+                                  linearization=general_config['linearization'],
+                                  max_in_length=general_config['max_in_len'],
+                                  max_out_length=general_config['max_out_len'])
+    print(f'Loaded validation data set of {len(valid_dataset)} instances.')
+
+    print("---------- Starting training ----------")
+    trainer = Trainer(model=model, args=train_config, train_dataset=train_dataset,
+                      eval_dataset=valid_dataset, data_collator=T2TDataCollator())
+    trainer.train()
+    print("---------- Finished training ----------")
+    print("---------- Saving model and tokenizer ----------")
+    trainer.save_model(train_config['output_dir'])
+    tokenizer.save_pretrained(train_config['output_dir'])
+
+
+if __name__=='__main__':
+
+    c = "./training_configs/training_config_3.json"
+
+    train_generation_model(c)
